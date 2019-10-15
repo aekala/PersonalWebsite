8cf08589c4ebeb822a291f0eb580a17e5bcdf0b6
Leo <lkkodish@gmail.com>
2019-10-15 12:52:04 -0400

    new files to test carousel

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".carousel-control-prev").click(function() {
+        $("#carousel").carousel("prev");
+    });
+    
+    $(".carousel-control-next").click(function() {
+        $("#carousel").carousel("next");
+    });
+});
+
