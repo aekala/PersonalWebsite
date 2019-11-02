261a2dc4a176177af767d03abab711d7b6738e68
Leo <lkkodish@gmail.com>
2019-11-02 10:09:30 -0400

    work experience

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ $(document).ready(function() {
     });
 });
 
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+});
+
+$(document).keydown(function(e) {       // move carousel on arrow key input
+    switch(e.which) {
+        case 37: // left arrow key
+            $("#carousel").carousel("prev");
+        break;
+
+        case 39: // right
+            $("#carousel").carousel("next");
+        break;
+
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
