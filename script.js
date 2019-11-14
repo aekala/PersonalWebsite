59f5c646029178904317f109c603ae107c1312dc
Leo <lkkodish@gmail.com>
2019-11-13 21:07:39 -0500

    whole lotta changes

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
-$(document).ready(function() {
-    $(".carousel-control-prev").click(function() {
-        $("#carousel").carousel("prev");
-    });
-    
-    $(".carousel-control-next").click(function() {
-        $("#carousel").carousel("next");
-    });
-});
-
-
 $(function () {
     $('[data-toggle="popover"]').popover()
 });
 
-$(document).keydown(function(e) {       // move carousel on arrow key input
-    switch(e.which) {
-        case 37: // left arrow key
-            $("#carousel").carousel("prev");
-        break;
-
-        case 39: // right
-            $("#carousel").carousel("next");
-        break;
 
 
-        default: return; // exit this handler for other keys
-    }
-    e.preventDefault(); // prevent the default action (scroll / move caret)
-});
+
